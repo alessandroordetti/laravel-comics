6299ba5551bc0bf12b961e1fbb150ff75464c6f7
alessandroordetti <alessandro.ordetti@gmail.com>
2022-05-04 16:57:31 +0200

    Inizializzazione esercizio

---
+++ Laravel-Comics/resources/views/guest/comics.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.base')
+
+@section('content')
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit quidem delectus sequi obcaecati fugit, quasi accusamus, fuga maiores officia, et eum repellendus soluta neque nostrum. Harum consequatur temporibus ipsum consectetur.</p>
+@endsection
---
+++ Laravel-Comics/resources/views/partials/header.blade.php
@@ -0,0 +1,60 @@
+<div class="header-wrapper">
+    <div class="img-wrapper">
+        <img src="{{asset("images/dc-logo.png")}}" alt="Logo DC">
+    </div>
+
+    <nav>
+        <ul>
+            <li>
+                <a href="">
+                    Characters
+                </a>
+            </li>
+            <li>
+                <a href="{{ route('comics-page')}}">
+                    Comics
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    Movie
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    Tv
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    Games
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    Collectibles
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    Videos
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    Fans
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    News
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    Shop
+                </a>
+            </li>
+        </ul>
+    </nav>
+</div>
