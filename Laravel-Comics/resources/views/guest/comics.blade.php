c66dbac2c1a9d69f43e88a2edbf7ff61ed1f9266
alessandroordetti <alessandro.ordetti@gmail.com>
2022-05-04 17:55:00 +0200

    Footer and Header completed

--- Laravel-Comics/resources/views/guest/comics.blade.php
+++ Laravel-Comics/resources/views/guest/comics.blade.php
@@ -1,5 +1,5 @@
 @extends('layout.base')
 
 @section('content')
-    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit quidem delectus sequi obcaecati fugit, quasi accusamus, fuga maiores officia, et eum repellendus soluta neque nostrum. Harum consequatur temporibus ipsum consectetur.</p>
+    <h1>Comics section</h1>
 @endsection
--- Laravel-Comics/resources/views/partials/footer.blade.php
+++ Laravel-Comics/resources/views/partials/footer.blade.php
@@ -1,1 +1,80 @@
-ciuao
+<div class="background">
+
+    <div class="contacts-wrapper">
+        <div class="contacts-1">
+            <h3>
+                DC COMICS
+            </h3>
+            <ul>
+                <li>Characters</li>
+                <li>Comics</li>
+                <li>Movies</li>
+                <li>TV</li>
+                <li>Games</li>
+                <li>Videos</li>
+                <li>News</li>
+            </ul>
+
+            <h3></h3>
+
+            <h3>SHOP</h3>
+            <ul>
+                <li>Shop DC</li>
+                <li>Shop DC Collectibles</li>
+            </ul>
+        </div>
+
+        <div class="contacts-2">
+            <h3>DC</h3>
+            <ul>
+                <li>Terms Of Use</li>
+                <li>Privacy policy (new)</li>
+                <li>Ad Choices</li>
+                <li>Advertising</li>
+                <li>Jobs</li>
+                <li>Subscriptions</li>
+                <li>Talent Workshop</li>
+                <li>CPSC Certificates</li>
+                <li>Ratings</li>
+                <li>Shop Help</li>
+                <li>Contact Us</li>
+            </ul>
+        </div>
+
+        <div class="contacts-3">
+            <h3>
+                Sites
+            </h3>
+
+            <ul>
+                <li>DC</li>
+                <li>MAD Magazine</li>
+                <li>Dc Kids</li>
+                <li>Dc Universe</li>
+                <li>Dc Power Visa</li>
+            </ul>
+        </div>
+
+        <div class="dc-bigger-logo">
+            <img src="{{asset("images/dc-logo-bg.png")}}" alt="">
+        </div>
+    </div>
+
+    <div class="footer-wrapper">
+        <div class="social-media-contacts">
+            <button>Sign-up now!</button>
+
+            <div>
+                <span>
+                    Follow Us
+                </span>
+
+                <a href="#"><img src="{{asset("images/footer-youtube.png")}}" alt="Logo Youtube"></a>
+                <a href="#"><img src="{{asset("images/footer-facebook.png")}}" alt="Logo Facebook"></a>
+                <a href="#"><img src="{{asset("images/footer-twitter.png")}}" alt="Logo Twitter"></a>
+                <a href="#"><img src="{{asset("images/footer-pinterest.png")}}" alt="Logo Pinterest"></a>
+                <a href="#"><img src="{{asset("images/footer-periscope.png")}}" alt="Logo Pinterest"></a>
+            </div>
+        </div>
+    </div>
+</div>
